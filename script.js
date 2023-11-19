0e7bffd04621f639ed11b85376a44dc438452ecd
fakhrynp <fakhrynp@gmail.com>
2023-11-19 19:50:18 +0700

    deploy website (fail)

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ ScrollReveal({
   ScrollReveal().reveal('.navbar', { delay: 5, origin: 'side' }); 
   ScrollReveal().reveal('.content', { delay: 650, origin: 'bottom' }); 
   ScrollReveal().reveal('.footer', { delay: 650, origin: 'top' }); 
+  ScrollReveal().reveal('.profile', { delay: 650, origin: 'top' }); 
+
+  document.getElementById('cardButton').addEventListener('click', function() {
+    document.getElementById('cardContainer').style.display = 'block';
+  });
+  
+  function closeCard() {
+    console.log('Close button clicked');
+    document.getElementById('cardContainer').style.display = 'none';
+  }
+  
+  
+  
