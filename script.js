d9b654bef149150bf9e9110f7cfba44eaa385f5f
fakhrynp <fakhrynp@gmail.com>
2023-11-19 18:17:21 +0700

    Deployment Final Project1 -dev

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+  });
+  
+  
+  ScrollReveal().reveal('.h1-object', { delay: 350 }); 
+  ScrollReveal().reveal('.navbar', { delay: 5, origin: 'side' }); 
+  ScrollReveal().reveal('.content', { delay: 650, origin: 'bottom' }); 
+  ScrollReveal().reveal('.footer', { delay: 650, origin: 'top' }); 
